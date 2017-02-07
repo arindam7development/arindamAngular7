a8c371c2a5979698f57ab506f0f22af409ed87f8
Arindam Mishra <arindam5191@gmail.com>
2017-02-07 12:10:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 weatherApp.config(function($routeProvider){
 	$routeProvider
 	.when('/',{
-		templateUrl:'/pages/home.htm',
+		templateUrl:'pages/home.htm',
 		controller:'homeController'
 	})
 	.when('/forecast',{
-		templateUrl:'/pages/forecast.htm',
+		templateUrl:'pages/forecast.htm',
 		controller:'forecastController'
 	})
 	
